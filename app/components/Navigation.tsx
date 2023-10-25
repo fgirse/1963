a1d259bd592a761df09bea1faafa52efbb10db24
Frank Girse <fgirse@mac-mini-von-frank.home>
2023-10-25 08:07:31 +0200

    9er commit

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import React from "react";  
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem,  Link, Button} from "@nextui-org/react"; 
-import AcmeLogo  from'../components/AcmeLogo';
-import { NavbarMenu, NavbarMenuToggle, NavbarMenuItem } from "@nextui-org/react";
-
+import React from "react";
+import {Navbar, NavbarMenuItem, NavbarMenuToggle, NavbarMenu ,NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import AcmeLogo from "../components/AcmeLogo";
 
 export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   const menuItems = [
     "Profile",
     "Dashboard",
@@ -21,59 +21,57 @@ export default function App() {
   ];
 
   return (
-    <Navbar disableAnimation isBordered>
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle />
-      </NavbarContent>
-
-      <NavbarContent className="sm:hidden pr-3" justify="center">
+    <Navbar className="bg-slate-700" onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent className="">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
         <NavbarBrand>
+          <Link href="./.">
+          <div className="w-16 md:w-24 lg:w-32">
           <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
+          </div>
+          </Link>
+          <p className="text-slate-400 font-bold text-inherit px-3">Andi Frank Mick</p>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand>
-          <AcmeLogo />
-          <p className="font-bold text-inherit">ACME</p>
-        </NavbarBrand>
         <NavbarItem>
           <Link color="foreground" href="#">
-            Features
+            Home
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
-          <Link href="#" aria-current="page" color="warning">
-            Customers
+          <Link href="#" aria-current="page">
+            Wohin?
           </Link>
         </NavbarItem>
         <NavbarItem>
           <Link color="foreground" href="#">
-            Integrations
+            Infos
           </Link>
         </NavbarItem>
       </NavbarContent>
-
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
           <Link href="#">Login</Link>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="warning" href="#" variant="flat">
+          <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up
           </Button>
         </NavbarItem>
       </NavbarContent>
-
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
-              className="w-full"
               color={
-                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
               }
+              className="w-full"
               href="#"
               size="lg"
             >
--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,19 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full mx-auto items-center justify-between font-mono text-sm lg:flex">¨
       <div className="mt-4 flex flex-col items-center justify-center">
       < div className="flex flex-col justify-center items-center">
-      <h1 className='mt-[12vh] leading-4 text-slate-800 text-center text-sm md:mt-[45vh] md:text-2xl lg:w-8/12'>&ldquo; es sind die menschlichen Begegnungen die das Leben lebenswert gestalten &ldquo;</h1>
+      <h1 className='mt-[15vh] leading-4 text-slate-800 text-center text-sm md:mt-[36vh] md:text-2xl lg:w-8/12'>&ldquo; es sind die menschlichen Begegnungen die das Leben lebenswert gestalten &ldquo;</h1>
       <h1 className='py-7 headingA text-amber-500 uppercase font-bowlbySC font-black text-6xl md:text-9xl lg:text-[12rem] xl:text-[15rem] text-center'>Einladung</h1>
       <h2 className="mb-5 text-amber-500 leading-6 font-black text-center w-[66vw] text-[1.45rem] md:text-4xl md-w-full font-bowlbySC lg:w-full  lg:text-8xl" >Samstag <br/> 18. November 2023 <br className='text-center' />17 Uhr</h2>
         <p className='py-4  text-white font-black w-72 text-sm text-justify md:text-[1.333rem] md:w-full'>Andi  09.November - Mick 21.Oktober - Frank  10.August. Ab diesem Datum haben wir drei also nun die sechs vor der null stehen!!! Mehr als Grund genug mit all denen die uns wichtig sind und die uns nahe stehen, diesen Zustand gebührend zu feiern.</p>
+        <h2 className="text-amber-500 text-3xl font-bold">Wohin?</h2> 
+        <p className='py-4  text-white font-black w-72 text-sm text-justify md:text-[1.333rem] md:w-full'>Veranstaltumgsort ist die Lichter Bar direkt am Flughafen/ Tower Bremgarten </p>
+        <h2 className="text-amber-500 text-3xl font-bold">Unterkunft?</h2> 
+        <p className='py-4  text-white font-black w-72 text-sm text-justify md:text-[1.333rem] md:w-full'>Ubernachtung: Hotelzimmer stehen am Flughafen Bremgarten zur Verfügung oder aber z.B in Heitersheim Hotel "OXX".</p>
+        <h2 className="text-amber-500 text-3xl font-bold">Anmeldung?</h2> 
+        <p className='py-4  text-white font-black w-72 text-sm text-justify md:text-[1.333rem] md:w-full'>Wir würden uns zwecks der Planung über ein kurzes Feedback ob oder ob nicht sehr freuen!</p>
+
+
+          
         </div>
       </div>  
 
