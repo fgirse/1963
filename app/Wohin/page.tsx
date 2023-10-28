1661cc48106fd74af9a54369e9eb673b6f10c67f
Frank Girse <fgirse@mac-mini-von-frank.home>
2023-10-28 22:43:27 +0200

    14er commit

--- app/Wohin/page.tsx
+++ app/Wohin/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import dynamic from 'next/dynamic'
+import Image from 'next/image'
 // import OpenStreetMap from '../component/OpenStreetMap'
 const OpenStreetMap = dynamic(() => import('../components/NavBar/OpenStreetMap'), {
   ssr: false,
@@ -9,11 +10,19 @@ const index = () => {
   return (
     <>
 
-    <section className='flex flex-col min-h-screen'>
-      <h1 className='text-center'>OpenStreetMap</h1>
+    <section className= ' w-full h-screen bg-slate-300 items-center bg-gradient-to-b from-slate-700 to-slate-300'>
+      <div className='flex min-h-full flex-col items-center justify-center'>
+        <div className='flex-1'>
+      <h1 className=' py-5  text-center text-slate-200 text-6xl'>GoogleMap</h1>
 
-      <div className='w-60 h-60 '>
-      <OpenStreetMap  />
+      <div className=' flex flex-col items-center justify-center bg-slate-200 border-8 border-slate-200 shadow-2xl mb-12'>
+      <Image src="/images/map.png" alt="Karte" width="600" height="600" className="mt-10" />
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <p className="py-3 mb-5  rounded-xl text-slate-50 text-center w-11/12 p-3 bg-slate-800"><span className="text-amber-600"> Anfahrt von Süden:</span> Autobahn A5: Ausfahrt Müllheim/Neuenburg: über Zinken und Griessheim in den Gewerbepark Breisgau-Hochschwarzwald</p>
+        <p className="py-3 text-slate-50 rounded-xl text-center w-11/12 bg-slate-800"><span className="text-amber-600"> Anfahrt von Norden:</span> Autobahn A5: Ausfahrt Bad Krozingen/ Breisach: über Hartheim und Bremgarten in den Gewerbepark Breisgau-Hochschwarzwald</p>
+      </div>
+      </div>
       </div>
       </section>
     </>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import SeoMeta from 'layout/partials/SeoMeta'
 import { Bowlby_One_SC } from"next/font/google";
 import config from "../app/config/config.json";
-
+ import Link  from'next/link'
 const bowlbySC = Bowlby_One_SC({
   subsets: ['latin'],
   weight: ['400',],
@@ -16,15 +16,17 @@ export default function Home() {
     <section className="h-[180vh]   w-full py-5 bg-gradient-to-b from-slate-700 to-slate-300 min-h-screen flex flex-col justify-between items-center">
         <main className="  bg-[url('/images/1963C.png')] bg-no-repeat bg-contain w-[100vw]  md-bg-contain lg:bg-[length:100%_96vh] ">
       < div className="flex flex-col justify-between items-center">
-      <h1 className='mt-[24vh] leading-4  text-center text-sm w-10/12 md:mt-[45vh] md:text-2xl  lg:font-bold  text-orange-500 lg:w-8/12 lg:px-24 lg:mt-[63vh] ' >&ldquo; es sind die menschlichen Begegnungen die das Leben lebenswert gestalten &ldquo;</h1>
+      <h1 className='mt-[28vh] leading-4  text-center text-sm w-10/12 md:mt-[45vh] md:text-2xl  lg:font-bold  text-orange-500 lg:w-8/12 lg:px-24 lg:mt-[63vh] ' >&ldquo; es sind die menschlichen Begegnungen die das Leben lebenswert gestalten &ldquo;</h1>
       <h1 className='py-7 headingG text-amber-500 uppercase font-bowlbySC font-black text-5xl md:text-9xl lg:text-[12rem] xl:text-[15rem] text-center'>Einladung</h1>
       <h2 className="mb-5 text-amber-500 leading-6 font-black text-center w-[66vw] text-[1.45rem] md:text-4xl md-w-full font-bowlbySC lg:w-full  lg:text-5xl" >Samstag <br/> 18. November 2023 <br className='text-center' />ab 18 Uhr</h2><br/>
       <p className="text-9xl">⚽</p>
       <p className=" text-center text-4xl text-amber-500  headingA " > &ldquo;Der Ball ist rund&ldquo;</p>
-        <p className=' py-5  text-white font-normal w-72 text-sm text-justify md:w-8/12 md:text-[1.333rem]'>Das Jahr 1963 hat Spuren hinterlassen: 10. August Frank - 21. Oktober Mick - 09. November  Andi und nicht zu vergessen die Geburtsstunde der Fussball Bundesliga am 24 . August 1963. <br/> &ldquo;Ich bin ein Berliner&ldquo; John F. Kennedy in Berlin - Beatle-Mania - Martin Luther King &ldquo;I have a dream &ldquo;... <br/> < br/> <hr/> < br/>
+        <p className=' py-5  text-white font-normal w-72 text-sm text-justify md:w-8/12 md:text-[1.333rem]'>Das Jahr 1963 hat Spuren hinterlassen: 10. August Frank - 21. Oktober Mick - 09. November  Andi und nicht zu vergessen die Geburtsstunde der Fussball Bundesliga am 24. August 1963. <br/> &ldquo;Ich bin ein Berliner&ldquo; John F. Kennedy in Berlin - Beatle-Mania - Martin Luther King &ldquo;I have a dream &ldquo;... <br/> < br/> <hr/> 
         Für uns Drei steht nun also die 6 vor der Null! Mehr als Grund genug mit all denen die uns wichtig sind diesen Zustand gebührend zu feiern. Unser Motto: Der Ball ist rund!</p><br/> < br/> <hr/> 
-        <h2 className="-mt- text-amber-500 text-3xl font-bold">Wohin?</h2> 
-        <p className='  text-white font-normal w-[75vw] text-sm text-justify md:text-[1.333rem] md:w-full'>Veranstaltungsort ist die FLYING-BAR direkt am Flughafen/ Tower Bremgarten Freiburger-Strasse</p><br/> < br/>< br/>
+        <h2 className=" text-amber-500 text-center text-3xl font-bold md:w-8/12">Wohin?</h2> 
+        <p className='text-center text-white font-normal w-[75vw] text-sm  md:text-[1.333rem] lg:w-full'>Veranstaltungsort ist die FLYING-BAR direkt am Flughafen/ Tower Bremgarten Freiburger-Strasse - siehe auch Karte unter</p><br/> < br/>< br/>
+        <Link href="/Wohin" className="mb-5 -mt-7 text-5xl px-5 py-2 text-white bg-slate-600 hover:bg-amber-500/50 border-2 hover:border-2 rounded-xl">Karte GoogleMaps</Link>
+        
         <h2 className=" text-amber-500 text-3xl font-normal">Anmeldung?</h2> 
         <p className=' py-4  text-white font-black w-72 text-xl text-justify md:text-[2.333rem] md:w-8/12 md:text-center lg:w-full'>Wer nid absagt isch dbyyy !!!!</p>
 
