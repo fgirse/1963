9f9a472e7741d48ad725dfe3acca4242fb15966c
Frank Girse <fgirse@mac-mini-von-frank.home>
2023-10-30 04:23:38 +0100

    20er commit

---
+++ app/components/Modals/Modal.js
@@ -0,0 +1,95 @@
+"use client"
+
+import React, { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+
+const Modal = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const trigger = useRef(null);
+  const modal = useRef(null);
+
+  // close on click outside
+  useEffect(() => {
+    const clickHandler = ({ target }) => {
+      if (!modal.current) return;
+      if (
+        !modalOpen ||
+        modal.current.contains(target) ||
+        trigger.current.contains(target)
+      )
+        return;
+      setModalOpen(false);
+    };
+    document.addEventListener("click", clickHandler);
+    return () => document.removeEventListener("click", clickHandler);
+  });
+
+  // close if the esc key is pressed
+  useEffect(() => {
+    const keyHandler = ({ keyCode }) => {
+      if (!modalOpen || keyCode !== 27) return;
+      setModalOpen(false);
+    };
+    document.addEventListener("keydown", keyHandler);
+    return () => document.removeEventListener("keydown", keyHandler);
+  });
+
+  return (
+    <>
+      <div className="container mx-auto py-20">
+        <button
+          ref={trigger}
+          onClick={() => setModalOpen(true)}
+          className={`px-6 py-3 text-xl font-medium text-white rounded-full bg-amber-500/50 hover:border-2 hover:bg-amber-500/90`}
+        >
+            Googel Map
+        </button>
+        <div
+          className={`fixed top-0 left-0 flex h-full min-h-screen w-full items-center justify-center bg-black bg-opacity-90 px-4 py-5 ${
+            modalOpen ? "block" : "hidden"
+          }`}
+        >
+          <div
+            ref={modal}
+            onFocus={() => setModalOpen(true)}
+            onBlur={() => setModalOpen(false)}
+            className="w-full max-w-[570px] rounded-[20px] bg-slate-400 py-12 px-8 text-center md:py-[60px] md:px-[70px]"
+          >
+        
+      
+      <h1 className=' py-5 text-center text-amber-500 text-6xl'>GoogleMap</h1>
+      <div className=' flex-1 flex flex-col items-center justify-center bg-slate-200 border-8 border-slate-200 shadow-2xl mb-12'>
+      <Image src="/images/Map.png" alt="Karte" width="300" height="300" className="mt-10" />
+      </div>
+      <div className=" mb-5 flex flex-col items-center justify-center">
+        <p className="py-3 px-2  mb-5  rounded-xl text-slate-50 text-center w-11/12 p-3 bg-slate-600"><span className="text-amber-600"> Anfahrt von Süden:</span> Autobahn A5: Ausfahrt Gewerbepark Breisgau/ Hartheim/ Heitersheim: über Bremgarten zum  südwestlichen Zugang des Gewerbeparkes.</p>
+        <p className=" py-3 px-2 text-slate-50 rounded-xl text-center w-11/12 bg-slate-600"><span className="text-amber-600"> Anfahrt von Norden:</span> Autobahn A5: Ausfahrt Bad Krozingen/ Breisach: über Hartheim und Bremgarten in den Gewerbepark Breisgau-Hochschwarzwald über den nördlichen ZUgang</p>
+      </div>
+      
+      
+      
+            <div className="flex flex-wrap -mx-3">
+              <div className="w-1/2 px-3">
+                <button
+                  onClick={() => setModalOpen(false)}
+                  className="block w-full rounded-lg border border-[#E9EDF9] p-3 text-center text-base font-medium text-dark transition hover:border-amber-600 hover:bg-slate-600  hover:text-white"
+                >
+                  zurück
+                </button>
+              </div>
+              <div className="w-1/2 px-3">
+               {/* <button
+                  className={`block w-full p-3 text-base font-medium text-center text-white transition border rounded-lg border-primary bg-slate-500 hover:bg-opacity-90`}
+                >
+                  <a href={`/#`}> View Details </a>
+        </button>*/}              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import Image from 'next/image'
 import SeoMeta from 'layout/partials/SeoMeta'
 import { Bowlby_One_SC } from"next/font/google";
 import config from "../app/config/config.json";
- import Link  from'next/link'
+import Modal from'..//app/components/Modals/Modal';
+import Link  from'next/link'
+
 const bowlbySC = Bowlby_One_SC({
   subsets: ['latin'],
   weight: ['400',],
@@ -25,17 +27,16 @@ export default function Home() {
       <h2 className="mb-5 m-auto  text-amber-500 leading-6 font-black text-center text-[1.45rem] md:text-4xl md-w-full font-bowlbySC lg:w-full  lg:text-5xl" >Samstag <br/> 18. November 2023 <br className='text-center' />ab 19.00 Uhr</h2><br/>
       <p className="m-auto text-9xl text-center">⚽</p>
       <p className="mt-3 text-center text-5xl text-amber-500  headingA " > &ldquo;Der Ball ist rund&ldquo;</p>
-      <p className=' py-5  text-white font-normal text-center text-sm  md:text-[1.333rem]'>Das Jahr 1963 hat Spuren hinterlassen: 10. August Frank - 21. Oktober Mick - 09. November  Andi und nicht zu vergessen die Geburtsstunde der Fussball Bundesliga am 24. August 1963.</p> <br/> 
-      <p className=" py-5  text-white font-normal text-center text-xl leading-5  md:text-[1.333rem]' ">&ldquo;Ich bin ein Berliner&ldquo; John F. Kennedy in Berlin - Beatle-Mania - Martin Luther King &ldquo;I have a dream &ldquo;...  </p>   
-      <p className=' py-5  text-white font-normal text-center text-sm  md:text-[1.333rem]'>Für uns Drei steht nun also die Sechs vor der Null! Mehr als Grund genug mit all denen die uns wichtig sind diesen Zustand gebührend zu feiern. Unser Motto: Der Ball ist rund!</p><br/> < br/> <hr/>
-      <p className="py-3 m-auto text-amber-500 text-center md:text-7xl font-bold md-text-5xl">Wohin?</p> 
-        <p className='text-center text-white font-normal w-[75vw] text-sm  md:text-[1.333rem] lg:w-full'>Veranstaltungsort ist die FLYING-BAR direkt am Flughafen/ Tower Bremgarten Freiburger-Strasse - siehe auch Karte unter</p><br/><br/>
-        <div className="flex flex-col justify-center items-center">
-        <Link href="/Wohin" className=" w-60 text-center  text-3xl md:text-4xl px-5 py-2 text-white bg-slate-600 hover:bg-amber-600/50 border-2 hover:border-2 rounded-xl ">Karte GoogleMaps</Link> 
-
+      <p className=' py-5  text-white text-center text-[1.0rem] md:text-[1.333rem]'>Das Jahr 1963 hat Spuren hinterlassen: 10. August Frank - 21. Oktober Mick - 09. November  Andi und nicht zu vergessen die Geburtsstunde der Fussball Bundesliga am 24. August 1963.</p> <br/> 
+      <p className=" py-5  text-white text-center text-[1.0rem]  md:text-[1.333rem]' ">&ldquo;Ich bin ein Berliner&ldquo; John F. Kennedy in Berlin - Beatle-Mania - Martin Luther King &ldquo;I have a dream &ldquo;...  </p>   
+      <p className=' py-5  text-white font-normal text-center text-[1.0rem] md:text-[1.333rem]'>Für uns Drei steht nun also die Sechs vor der Null! Mehr als Grund genug mit all denen die uns wichtig sind diesen Zustand gebührend zu feiern. Unser Motto: Der Ball ist rund!</p><br/> < br/> <hr/>
+      <p className="py-3 m-auto text-5xl text-amber-500 text-center md:text-7xl font-bold md-text-5xl">Wohin?</p> 
+        <p className='text-center text-white font-normal text-xl  md:text-[1.333rem] lg:w-full'>Veranstaltungsort ist die <span className="text-amber-600"> FLYING-BAR </span>direkt am Flughafen/ Tower Bremgarten Freiburger-Strasse - siehe auch Karte unten - click</p>
+        <div className="flex flex-col">
+                <Modal/>
         </div>
       <h2 className="py-9  text-amber-500 text-center text-5xl font-normal">Anmeldung?</h2> 
-        <p className='m-auto text-white font-black text-xl text-center md:text-[2.333rem] md:w-8/12 md:text-center lg:w-full'>Wer nid absagt isch dbyyy !!!!</p>
+        <p className='m-auto text-white font-black text-[1.0rem] text-center md:text-[2.333rem] md:w-8/12 md:text-center lg:w-full'>Wer nid absagt isch dbyyy !!!!</p>
       </main>
       </section>
     </>
